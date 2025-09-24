7742ff7f30dfd952c6a5501655a2996bcb4db0b9
ManuelMNJ <manuelangelnietojimenez@gmail.com>
2025-09-24 11:09:19 +0200

    cliente

--- Cliente/JS/pr3/js.js
+++ Cliente/JS/pr3/js.js
@@ -8,7 +8,6 @@ function pedirNotas() {
     while (isNaN(nota1) || nota1 === "" || nota1 === null || nota1 < 0 || nota1 > 10) {
         nota1 = prompt("Ingresa la primera nota:");
     }
-
     while (isNaN(nota2) || nota2 === "" || nota2 === null || nota2 < 0 || nota2 > 10) {
         nota2 = prompt("Ingresa la segunda nota:");
     }
@@ -32,11 +31,13 @@ function calcularMedia(n1, n2, n3, n4) {
     } else if (media >= 6 && media < 7) {
         p.innerHTML = `El alumno ha sacado un: <br/>${media} (Bien)`;
     } else if (media >= 7 && media < 9) {
-        p.innerHTML = `El alumno ha sacado un: <br/>${media} (Notable)`;
+        p.i nnerHTML = `El alumno ha sacado un: <br/>${media} (Notable)`;
     } else if (media >= 9 && media <= 10) {
         p.innerHTML = `El alumno ha sacado un: <br/>${media} (Sobresaliente)`;
     }
 }
 
-pedirNotas();
+document.addEventListener("DOMContentLoaded", () => {
+pedirNotas()});
+
 calcularMedia(nota1, nota2, nota3, nota4);
